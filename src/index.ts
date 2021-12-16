99cabec4657383fc48276c31434bb9075cbe907e
Sara Morillon <sara.morillon.pro@gmail.com>
2021-12-16 16:43:30 +0100

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { useFetch } from './useFetch'
-import { useBoolean, useMultiSelect, useNumber, useSelect, useText } from './useFields'
-import { usePagination } from './usePagination'
-import { useTheme } from './useTheme'
-
-const fields = {
-  useText,
-  useBoolean,
-  useMultiSelect,
-  useNumber,
-  useSelect,
-}
-
-export { useFetch, fields, useTheme, usePagination }
+export * from './useFetch'
+export * as fields from './useFields'
+export * from './usePagination'
+export * from './useTheme'
