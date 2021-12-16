3c9276231d458b7aa58da36ae2094a542501d46c
Sara Morillon <sara.morillon-ext@kering.com>
2021-12-16 12:35:15 +0100

    Add usePagination hook

---
+++ src/usePagination.ts
@@ -0,0 +1,47 @@
+import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react'
+
+export interface IPagination {
+  page: number
+  maxPage: number
+  setMaxPage: Dispatch<SetStateAction<number>>
+  goTo: Dispatch<SetStateAction<number>>
+  first: () => void
+  previous: () => void
+  next: () => void
+  last: () => void
+  canPrevious: boolean
+  canNext: boolean
+}
+
+export function usePagination(): IPagination {
+  const [maxPage, setMaxPage] = useState(1)
+  const [page, setPage] = useState(1)
+
+  const goTo = useCallback(
+    (action: number | ((page: number) => number)) => {
+      setPage((page) => Math.max(1, Math.min(typeof action === 'number' ? action : action(page), maxPage)))
+    },
+    [maxPage]
+  )
+
+  const first = useCallback(() => {
+    goTo(1)
+  }, [goTo])
+
+  const previous = useCallback(() => {
+    goTo((page) => page - 1)
+  }, [goTo])
+
+  const next = useCallback(() => {
+    goTo((page) => page + 1)
+  }, [goTo])
+
+  const last = useCallback(() => {
+    goTo(maxPage)
+  }, [goTo, maxPage])
+
+  const canPrevious = useMemo(() => page > 1, [page])
+  const canNext = useMemo(() => page < maxPage, [page, maxPage])
+
+  return { page, maxPage, setMaxPage, goTo, first, previous, next, last, canPrevious, canNext }
+}
---
+++ tests/unit/usePagination.test.ts
@@ -0,0 +1,143 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import { usePagination } from '../../src/usePagination'
+
+describe('page', () => {
+  it('should return page 1 by default', () => {
+    const { result } = renderHook(() => usePagination())
+    expect(result.current.page).toBe(1)
+  })
+})
+
+describe('maxPages', () => {
+  it('should return max page 1 by default', () => {
+    const { result } = renderHook(() => usePagination())
+    expect(result.current.maxPage).toBe(1)
+  })
+})
+
+describe('setMaxPages', () => {
+  it('should set max page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    expect(result.current.maxPage).toBe(2)
+  })
+})
+
+describe('goto', () => {
+  it('should go to page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo(2))
+    expect(result.current.page).toBe(2)
+  })
+
+  it('should not go to page lower than 1', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.goTo(0))
+    expect(result.current.page).toBe(1)
+  })
+
+  it('should not go to page higher than max page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.goTo(2))
+    expect(result.current.page).toBe(1)
+  })
+
+  it('should go to page using function', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo((page) => page + 1))
+    expect(result.current.page).toBe(2)
+  })
+
+  it('should not go to page lower than 1 using function', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.goTo((page) => page - 1))
+    expect(result.current.page).toBe(1)
+  })
+
+  it('should not go to page higher than max page using function', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.goTo((page) => page + 1))
+    expect(result.current.page).toBe(1)
+  })
+})
+
+describe('first', () => {
+  it('should go to first page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo(2))
+    act(() => result.current.first())
+    expect(result.current.page).toBe(1)
+  })
+})
+
+describe('previous', () => {
+  it('should go to previous page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo(2))
+    act(() => result.current.previous())
+    expect(result.current.page).toBe(1)
+  })
+
+  it('should not go to previous page if page is 1', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.previous())
+    expect(result.current.page).toBe(1)
+  })
+})
+
+describe('next', () => {
+  it('should go to next page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.next())
+    expect(result.current.page).toBe(2)
+  })
+
+  it('should not go to next page if page is max page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.next())
+    expect(result.current.page).toBe(1)
+  })
+})
+
+describe('last', () => {
+  it('should go to last page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.last())
+    expect(result.current.page).toBe(2)
+  })
+})
+
+describe('canPrevious', () => {
+  it('should return true if page is higher than 1', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo(2))
+    expect(result.current.canPrevious).toBe(true)
+  })
+
+  it('should return false if page is lower or equal than 1', () => {
+    const { result } = renderHook(() => usePagination())
+    expect(result.current.canPrevious).toBe(false)
+  })
+})
+
+describe('canNext', () => {
+  it('should return true if page is lower than max page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    expect(result.current.canNext).toBe(true)
+  })
+
+  it('should return false if page is higher or equal than max page', () => {
+    const { result } = renderHook(() => usePagination())
+    act(() => result.current.setMaxPage(2))
+    act(() => result.current.goTo(2))
+    expect(result.current.canNext).toBe(false)
+  })
+})
