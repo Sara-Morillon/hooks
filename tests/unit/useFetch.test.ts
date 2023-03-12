7921753ac31dda833c29dd46e355d463157d406d
Sara Morillon <sara.morillon.pro@gmail.com>
2023-03-12 22:41:05 +0100

    Fix test on fetch hook

--- tests/unit/useFetch.test.ts
+++ tests/unit/useFetch.test.ts
@@ -1,6 +1,6 @@
 import { act, renderHook } from '@testing-library/react'
 import { useFetch } from '../../src/useFetch'
-import { mockPromiseChain } from '../mock'
+import { flushPromises, mockPromiseChain } from '../mock'
 
 describe('useFetch', () => {
   it('should use default value', () => {
@@ -23,8 +23,8 @@ describe('useFetch', () => {
 
   it('should fetch data', async () => {
     const fetchFn = jest.fn().mockResolvedValue('result')
-    const { waitForNextUpdate } = renderHook(() => useFetch(fetchFn, 'default'))
-    await waitForNextUpdate()
+    renderHook(() => useFetch(fetchFn, 'default'))
+    await flushPromises()
     expect(fetchFn).toHaveBeenCalled()
   })
 
@@ -36,58 +36,58 @@ describe('useFetch', () => {
 
   it('should use value returned by fetch', async () => {
     const fetchFn = jest.fn().mockResolvedValue('result')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, 'default'))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, 'default'))
+    await flushPromises()
     expect(result.current[0]).toBe('result')
   })
 
   it('should not be loading after fetch', async () => {
     const fetchFn = jest.fn().mockResolvedValue('')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     expect(result.current[1].loading).toBe(false)
   })
 
   it('should not have error if fetch succeeds', async () => {
     const fetchFn = jest.fn().mockResolvedValue('')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     expect(result.current[1].error).toBeUndefined()
   })
 
   it('should have error if fetch fails', async () => {
     const fetchFn = jest.fn().mockRejectedValue(new Error('500'))
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     expect(result.current[1].error).toEqual(new Error('500'))
   })
 
   it('should be loading when refreshing', async () => {
     const fetchFn = jest.fn().mockResolvedValue('')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     act(() => {
       result.current[2]()
     })
     expect(result.current[1].loading).toBe(true)
-    await waitForNextUpdate()
+    await flushPromises()
   })
 
   it('should fetch data when replaying action', async () => {
     const fetchFn = jest.fn().mockResolvedValue('')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     act(() => {
       result.current[2]()
     })
     expect(fetchFn).toHaveBeenCalled()
-    await waitForNextUpdate()
+    await flushPromises()
   })
 
   it('should replace data', async () => {
     const fetchFn = jest.fn().mockResolvedValue('')
-    const { result, waitForNextUpdate } = renderHook(() => useFetch(fetchFn, ''))
-    await waitForNextUpdate()
+    const { result } = renderHook(() => useFetch(fetchFn, ''))
+    await flushPromises()
     act(() => {
       result.current[3]('toto')
     })
