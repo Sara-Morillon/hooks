6aa9cce02bd5577805ef3e87e95f4184864327ce
Sara Morillon <sara.morillon.pro@gmail.com>
2023-04-22 11:55:54 +0200

    Add useDialog

---
+++ src/useDialog.ts
@@ -0,0 +1,30 @@
+import { RefObject, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+
+export interface IDialog {
+  ref: RefObject<HTMLDialogElement>
+  visible: boolean
+  show: () => void
+  hide: () => void
+}
+
+export function useDialog(): IDialog {
+  const ref = useRef<HTMLDialogElement>(null)
+  const [visible, setVisible] = useState(false)
+
+  const show = useCallback(() => {
+    setVisible(true)
+  }, [])
+
+  const hide = useCallback(() => {
+    setVisible(false)
+  }, [])
+
+  useEffect(() => {
+    if (ref.current) {
+      if (visible) ref.current.showModal()
+      else ref.current.close()
+    }
+  }, [visible])
+
+  return useMemo(() => ({ ref, visible, show, hide }), [visible, show, hide])
+}
