f395b96b7912a873b65b2e3f3cffc7e500b76397
Sara Morillon <sara.morillon.pro@gmail.com>
2024-12-17 14:59:21 +0100

    Fix default loading value (#1)

--- tests/unit/useFetch.test.ts
+++ tests/unit/useFetch.test.ts
@@ -15,6 +15,12 @@ describe('useFetch', () => {
     expect(result.current[1].loading).toBe(true)
   })
 
+  it('should not be loading by default if auto fetch is false', () => {
+    const fetchFn = mockPromiseChain()
+    const { result } = renderHook(() => useFetch(fetchFn, '', false))
+    expect(result.current[1].loading).toBe(false)
+  })
+
   it('should not have error by default', () => {
     const fetchFn = mockPromiseChain()
     const { result } = renderHook(() => useFetch(fetchFn, ''))
