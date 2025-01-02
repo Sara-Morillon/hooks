cdbb7abfe5f58e3d85cf8c917f7ec3f1458712c5
Sara Morillon <sara.morillon.pro@gmail.com>
2025-01-02 12:50:46 +0100

    Allow use of functions in debounce

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 
 export function useDebounce<T>(value: T, delay = 500): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+  const [debouncedValue, setDebouncedValue] = useState<T>(() => value)
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay)
+    const timer = setTimeout(() => setDebouncedValue(() => value), delay)
     return () => {
       clearTimeout(timer)
     }
