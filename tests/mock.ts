d92c0bb46beeaab93505b433ce7bd21d570bc2a6
Sara Morillon <sara.morillon.pro@gmail.com>
2025-02-12 13:07:42 +0100

    Add custom sort functions

--- tests/mock.ts
+++ tests/mock.ts
@@ -40,26 +40,31 @@ export function flushPromises() {
 export interface IData {
   name: string
   age: number
+  address: {
+    number: number
+    street: string
+    city: string
+  }
 }
 
 export function mockTableData(): IData[] {
   return [
-    { name: 'Zola Ray', age: 30 },
-    { name: 'Arlo Curtis', age: 32 },
-    { name: 'Alexis West', age: 39 },
-    { name: 'Diego Fitzpatrick', age: 40 },
-    { name: 'Annabella Vargas', age: 37 },
-    { name: 'Ryker Gregory', age: 34 },
-    { name: 'Alaya Winters', age: 40 },
-    { name: 'Deandre Vu', age: 32 },
-    { name: 'Kimora Higgins', age: 38 },
-    { name: 'Sterling Pugh', age: 30 },
-    { name: 'Landry Pineda', age: 31 },
-    { name: 'Gerardo Pearson', age: 39 },
-    { name: 'Kiara Nava', age: 34 },
-    { name: 'Stefan Hart', age: 35 },
-    { name: 'Gemma Terrell', age: 38 },
-    { name: 'Jaxen Smith', age: 35 },
-    { name: 'Olivia Camacho', age: 30 },
+    { name: 'Zola Ray', age: 30, address: { number: 47, street: 'Crescent Avenue', city: 'Druimkinnerras' } },
+    { name: 'Arlo Curtis', age: 32, address: { number: 50, street: 'Iolaire Road', city: 'New Inn' } },
+    { name: 'Alexis West', age: 39, address: { number: 6, street: 'Overton Circle', city: 'Little Woolstone' } },
+    { name: 'Diego Fitzpatrick', age: 40, address: { number: 6, street: 'Witney Way', city: 'Kirtling Green' } },
+    { name: 'Annabella Vargas', age: 37, address: { number: 13, street: 'Sandyhill Rd', city: 'Frostenden' } },
+    { name: 'Ryker Gregory', age: 34, address: { number: 41, street: 'Hounslow Rd', city: 'Sopworth' } },
+    { name: 'Alaya Winters', age: 40, address: { number: 67, street: 'Harehills Lane', city: 'Rowley' } },
+    { name: 'Deandre Vu', age: 32, address: { number: 12, street: 'Warner Close', city: 'Haslington' } },
+    { name: 'Kimora Higgins', age: 38, address: { number: 97, street: 'Wade Lane', city: 'Sandaig' } },
+    { name: 'Sterling Pugh', age: 30, address: { number: 11, street: 'Guildry Street', city: 'Gartavaich' } },
+    { name: 'Landry Pineda', age: 31, address: { number: 49, street: 'North Promenade', city: 'Dowland' } },
+    { name: 'Gerardo Pearson', age: 39, address: { number: 19, street: 'Annfield Rd', city: 'Beckley' } },
+    { name: 'Kiara Nava', age: 34, address: { number: 96, street: 'Cunnery Rd', city: 'Malling' } },
+    { name: 'Stefan Hart', age: 35, address: { number: 31, street: 'South Crescent', city: 'Lynemore' } },
+    { name: 'Gemma Terrell', age: 38, address: { number: 19, street: 'Ilchester Road', city: 'Myddfai' } },
+    { name: 'Jaxen Smith', age: 35, address: { number: 42, street: 'Folkestone Road', city: 'Winslade' } },
+    { name: 'Olivia Camacho', age: 30, address: { number: 19, street: 'Well Lane', city: 'Parkham' } },
   ]
 }
