b91f983c0d9b49bd247fa0a3d6b9dc93354b0fa9
Sara Morillon <sara.morillon.pro@gmail.com>
2025-12-22 13:30:22 +0100

    Do not use useAction for useFetch

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,15 +1,48 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { useAction } from './useAction.js'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 export type IFetchResult<T> = [data: T, state: { loading: boolean; error?: unknown }, refresh: () => Promise<void>]
 
 export function useFetch<T>(fetch: (signal: AbortSignal) => Promise<T>, defaultValue: T): IFetchResult<T>
 export function useFetch<T>(fetch: (signal: AbortSignal) => Promise<T>): IFetchResult<T | undefined>
 export function useFetch<T>(fetch: (signal: AbortSignal) => Promise<T>, defaultValue?: T): IFetchResult<T | undefined> {
-  const [{ loading, error }, execute] = useAction(fetch)
+  const controllerRef = useRef<AbortController | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<unknown>()
   const [data, setData] = useState(defaultValue)
 
-  const refresh = useCallback(() => execute().then(setData), [execute])
+  const refresh = useCallback(async () => {
+    if (controllerRef.current) {
+      controllerRef.current.abort()
+    }
+
+    const controller = new AbortController()
+    controllerRef.current = controller
+
+    setLoading(true)
+    setError(undefined)
+    try {
+      const data = await fetch(controller.signal)
+      if (!controller.signal.aborted) {
+        setData(data)
+      }
+    } catch (error) {
+      if (!(error instanceof DOMException && error.name === 'AbortError')) {
+        setError(error)
+      }
+    } finally {
+      if (!controller.signal.aborted) {
+        setLoading(false)
+      }
+    }
+  }, [fetch])
+
+  useEffect(() => {
+    return () => {
+      if (controllerRef.current) {
+        controllerRef.current.abort()
+      }
+    }
+  }, [])
 
   useEffect(() => {
     refresh()
